aa043f34b0731deb2f342c3aa615b75f65372210
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-30 18:23:30 -0500

    Implemented: simple sync events.

---
+++ test/tally_ho.js
@@ -0,0 +1,28 @@
+
+var _ = require("underscore")._,
+assert = require("assert"),
+
+Tally_Ho = require("../lib/tally_ho").Tally_Ho
+;
+
+Tally_Ho.on('add', function (o) {
+  o.data.result.push(1);
+});
+
+Tally_Ho.on('add', function (o) {
+  o.data.result.push(2);
+});
+
+
+describe( 'Tally_Ho', function () {
+
+  it( 'runs events in defined order', function (done) {
+    process.nextTick(function () {
+      Tally_Ho.emit('add', {result: []}, function (o) {
+        assert.deepEqual( o.data.result, [1,2]);
+        done();
+      })
+    });
+  });
+
+}); // === end desc
