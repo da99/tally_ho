1f9d45e32e557e8554b358bb8c046afd22327621
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-31 02:35:38 -0500

    Added: .run, for async-like functionality

---
+++ test/05-run.js
@@ -0,0 +1,37 @@
+
+var _      = require("underscore")._
+  , assert = require("assert")
+  , One    = require("../lib/tally_ho").Tally_Ho.new()
+  , Two    = require("../lib/tally_ho").Tally_Ho.new()
+  , Third  = require("../lib/tally_ho").Tally_Ho.new()
+;
+
+describe( 'run', function () {
+
+  it( 'runs functions in sequential order', function () {
+    var d = [];
+    One.run(
+      function (o) { d.push(1); o.finish(); },
+      function (o) { d.push(2); o.finish(); }
+    );
+    assert.deepEqual(d, [1,2]);
+  });
+
+  it( 'finishes parent when functions are done', function () {
+    var d = [];
+
+    One.on('finishes parent', function (o) {
+      d.push(1);
+      Two.run(
+        o,
+        function (o) { d.push(2); o.finish(); },
+        function (o) { d.push(3); o.finish(); }
+     );
+    });
+
+    One.emit('finishes parent');
+
+    assert.deepEqual(d, [1,2,3]);
+  });
+
+}); // === end desc
