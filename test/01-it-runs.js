dc6de2cc87571b2c0771687e3f7f43919ec928ec
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-31 16:21:24 -0500

    Added: multiple data objects are merged into one.

--- test/01-it-runs.js
+++ test/01-it-runs.js
@@ -2,6 +2,7 @@
 var _        = require("underscore")._
   , assert   = require("assert")
   , Tally_Ho = require("../lib/tally_ho").Tally_Ho.new()
+  , F        = require("../lib/tally_ho").Tally_Ho.new()
 ;
 
 Tally_Ho.on('add', function (o) {
@@ -20,7 +21,7 @@ Tally_Ho.on('mult', 'div', function (o) {
 });
 
 
-describe( 'Tally_Ho', function () {
+describe( '.run', function () {
 
   it( 'runs events in defined order', function (done) {
     process.nextTick(function () {
@@ -42,6 +43,24 @@ describe( 'Tally_Ho', function () {
         done();
       })
     });
+  });
+
+  it( 'combines data objects into one object', function () {
+    var t = F;
+    var o = {};
+    t.on('one', function (d) {
+      _.extend(o, d.data, {two: 'b'});
+      d.finish();
+    });
+
+    t.on('one', function (d) {
+      _.extend(o, d.data, {three: 'c'});
+      d.finish();
+    });
+
+    t.run('one', {zero: 0}, {one: 1});
+
+    assert.deepEqual(o, {zero: 0, one: 1, two: 'b', three: 'c'});
   });
 
 }); // === end desc
