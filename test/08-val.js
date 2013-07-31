07a16e26fa6089e15cae82fe301afc0df2d2c6fb
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-31 04:35:13 -0500

    Added: .val. Fixed: .finished

---
+++ test/08-val.js
@@ -0,0 +1,21 @@
+
+var _      = require("underscore")._
+  , assert = require("assert")
+  , One    = require("../lib/tally_ho").Tally_Ho.new()
+;
+
+describe( 'val', function () {
+
+  it( 'does not get updated with .finish()', function () {
+    var v = 0;
+
+    One.on('one', function (o) { o.finish(1); });
+
+    One.on('after one', function (o) { o.finish(); });
+
+    One.emit('one', function (o) { v = o.val; });
+
+    assert.equal(v, 1);
+  });
+
+}); // === end desc
