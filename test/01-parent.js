6ad9cfdbf21af22243742111d9653b66c4a5b4f3
da99 <i-hate-spam-1234567@mailinator.com>
2013-07-30 23:20:23 -0500

    Added/Fixed: last callback run after nested children finished.

---
+++ test/01-parent.js
@@ -0,0 +1,41 @@
+
+var _      = require("underscore")._
+  , assert = require("assert")
+  , First  = require("../lib/tally_ho").Tally_Ho.new()
+  , Sec    = require("../lib/tally_ho").Tally_Ho.new()
+;
+
+
+First.on('add', function (o) {
+  o.data.result.push(o.run.event_name);
+  First.emit(o, 'sub', o.data);
+});
+
+First.on('sub', function (o) {
+  o.data.result.push(o.run.event_name);
+  Sec.emit(o, 'multi', o.data);
+});
+
+Sec.on('multi', function (o) {
+  o.data.result.push(o.run.event_name);
+  Sec.emit(o, 'div', o.data);
+});
+
+Sec.on('div', function (o) {
+  o.data.result.push(o.run.event_name);
+  o.finish();
+});
+
+describe( 'parent', function () {
+
+  it( 'runs last callback after nested children are finished', function (done) {
+    process.nextTick(function () {
+      var o = {result: []};
+      First.emit('add', o, function () {
+        assert.deepEqual(o.result, ['add', 'sub', 'multi', 'div']);
+        done();
+      });
+    });
+  });
+
+}); // === end desc
