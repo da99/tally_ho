c63ba5447de1a7e3baf7d6b98ac136c5cb53213d
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-04 09:25:53 -0500

    Added: .includes used when running.

--- test/01-it-runs.js
+++ test/01-it-runs.js
@@ -24,28 +24,10 @@ T.on('mult', 'div', function (o) {
 
 describe( '.run', function () {
 
-  it( 'prepends arguments in specified order to .includes', function () {
-    var t1 = Tally_Ho.new();
-    t1._val = 1;
-
-    var t2 = Tally_Ho.new();
-    t2._val = 2;
-
-    var t3 = Tally_Ho.new(t1, t2);
-    assert.equal(t3.includes[0]._val, t1._val);
-    assert.equal(t3.includes[1]._val, t2._val);
-  });
-
-  it( 'filters out duplicates among arguments in .includes', function () {
-    var t1 = Tally_Ho.new();
-    var t2 = Tally_Ho.new(t1, t1, t1);
-    assert.equal(t2.includes.length, 2);
-  });
-
   it( 'runs events in defined order', function (done) {
     process.nextTick(function () {
       T.run('add', {result: []}, function (o) {
-        assert.deepEqual( o.data.result, [1,2]);
+        assert.deepEqual(o.data.result, [1,2]);
         done();
       })
     });
@@ -83,3 +65,47 @@ describe( '.run', function () {
   });
 
 }); // === end desc
+
+
+
+describe( '.run .includes', function () {
+  it( 'prepends arguments in specified order to .includes', function () {
+    var t1 = Tally_Ho.new();
+    t1._val = 1;
+
+    var t2 = Tally_Ho.new();
+    t2._val = 2;
+
+    var t3 = Tally_Ho.new(t1, t2);
+    assert.equal(t3.includes[0]._val, t1._val);
+    assert.equal(t3.includes[1]._val, t2._val);
+  });
+
+  it( 'filters out duplicates among arguments in .includes', function () {
+    var t1 = Tally_Ho.new();
+    var t2 = Tally_Ho.new(t1, t1, t1);
+    assert.equal(t2.includes.length, 2);
+  });
+
+  it( 'runs events in .includes', function () {
+    var t1 = Tally_Ho.new();
+    t1.on('one', function (f) { f.data.vals.push(1); f.finish(); });
+    t1.on('two', function (f) { f.data.vals.push(2); f.finish(); });
+
+    var t2 = Tally_Ho.new(t1, t1, t1);
+    t2.on('one', function (f) { f.data.vals.push(3); f.finish(); });
+    t2.on('two', function (f) { f.data.vals.push(4); f.finish(); });
+
+    var o = {vals: []};
+    t2.run('one', 'two', o);
+    assert.deepEqual(o, {vals: [1,3,2,4]});
+  });
+
+}); // === end desc
+
+
+
+
+
+
+
